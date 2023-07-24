a08f716a24a87cb4d864013ec86b5fb2bdf23026
Crowdbotics <team@crowdbotics.com>
2023-07-24 08:32:28 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "virgo-42381",
+  url: "https://virgo-42381.botics.co",
+  api: "https://virgo-42381.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
